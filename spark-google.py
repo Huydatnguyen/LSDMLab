b62ab40b600c6a9b9a8c214f1c65bc078297c6f5
NourElh <hn_el_hassane@esi.dz>
2022-12-15 21:47:48 +0100

    update question 6

--- spark-google.py
+++ spark-google.py
@@ -112,10 +112,12 @@ def main():
 
 			# keep the RDD in memory
 			task_events.cache()
+			
 			filtered=task_events.filter(lambda t: t[9]!='' and t[10]!='' and t[11]!='') # remove tasks whose resource requests are not specified
 			# extract tasks that request big resources (CPU, RAM, local disk resource request > 0.5)
 			tasks_big=filtered.filter(lambda t: float(t[9])>0.5 or float(t[10])>0.5 or float(t[11])>0.5) 
-			tasks_big=tasks_big.map(lambda t: (t[2]+t[3])).distinct()
+			tasks_big=tasks_big.map(lambda t: (t[2]+t[3],1)).distinct()
+			# print('requests: ',tasks_big)
 
 			# read the input files into an RDD[String]
 			resource_usage = sc.parallelize([])
@@ -123,15 +125,24 @@ def main():
 			for file in files:
 				rsc_usage_rdd = sc.textFile("resource_usage/"+file)
 				resource_usage = resource_usage.union(rsc_usage_rdd)
+			# split each line into an array of items
+			resource_usage = resource_usage.map(lambda x : x.split(','))
 
-			filtered=resource_usage.filter(lambda t: t[5]!='' and t[6]!='' and t[12]!='')
+			# keep the RDD in memory
+			resource_usage.cache()
+
+			filtered_2=resource_usage.filter(lambda t: t[5]!='' and t[6]!='' and t[12]!='')
 			# extract tasks that consume big resources
-			rsc_big=filtered.filter(lambda t: float(t[5])>0.5 or float(t[6])>0.5 or float(t[12])>0.5).map(lambda t: (t[2]+t[3])).distinct()
+			rsc_big=filtered_2.filter(lambda t: float(t[5])>0.5 or float(t[6])>0.5 or float(t[12])>0.5)
+			rsc_big=rsc_big.map(lambda t: (t[2]+t[3],1)).distinct()
+			# print('usage: ',rsc_big)
+
 			nb_tasks_big_rsc=rsc_big.join(tasks_big).count()
 			print("Percentage of tasks consuming big resources among those requesting big resources: ",nb_tasks_big_rsc/rsc_big.count())
 
 		elif val==7:
 		# 7. Can we observe correlations between peaks of high resource consumption on some machines and task eviction events?
+			
 			# read the input files into an RDD[String]
 			resource_usage = sc.parallelize([])
 			files=os.listdir("resource_usage/")
@@ -199,6 +210,8 @@ def main():
 			plt.show()
 		
 		elif val==9:
+		# 9. What are hardware specifications of machines on which different priority tasks have/haven't successfully run?
+			
 			# read the input files into an RDD[String]
 			task_events = sc.parallelize([])
 			files=os.listdir("task_events/")
